d1a34d30c95868bfec21bfc61bc335e20c2a021a
Robert Righter <robrighter@gmail.com>
2010-08-13 11:31:18 -0400

    Bug fix with parsed floats

--- currentcost.js
+++ currentcost.js
@@ -9,9 +9,14 @@
     var that = this;
     var dataprocess = spawn("python", ["./python/tailserial.py", device]);
     dataprocess.stdout.on('data', function (data) {
-      var tosend = formatData(xml.parse(new String(data), "", false));
-      if(tosend)
-        that.emit(( tosend.hasOwnProperty('hist')? 'history' : 'incremental' ), tosend);
+      try{
+        var tosend = formatData(xml.parse(new String(data), "", false));
+        if(tosend)
+          that.emit(( tosend.hasOwnProperty('hist')? 'history' : 'incremental' ), tosend);
+      }
+      catch(err){
+        that.emit('error', data+"");
+      }
     });
     dataprocess.stderr.on('data', function (data) {
       that.emit('error', data+"");
